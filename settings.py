9295b69f1559d704ae8ecc7ac5fb9e1131467e7a
Marcos José <marcosjoseDV@hotmail.com>
2024-03-16 19:28:58 -0300

    Resumo,
    Definindo configurações padrões e cores.

---
+++ settings.py
@@ -0,0 +1,20 @@
+# default values
+ROTATE_DEFAULT = 0
+ZOOM_DEFOULT = 0
+FLIP_OPTIONS = ['None', 'X', 'Y', 'Both']
+BLUR_DEFAULT = 0
+CONTRAST_DEAULT = 0
+EFFECT_OPTIONS = ['Nome', 'emboss', 'Find edges', 'Contour', 'Edger enhance']
+BRIGHTNESS_DEFAULT = 1
+VIBRANCE_DEFAULT = 1
+GRAYSCALE_DEFAULT = False
+INVERT_DEFAULT = False
+
+
+# Colors
+WHITE = '#FFF'
+GREY = 'grey'
+PURPLE = '#4D446F'
+DARK_GREY = "#4a4a4a"
+CLOSE_RED = '#8a0606'
+SLIDER_BG = '#64686b'
